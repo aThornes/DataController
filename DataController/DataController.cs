e9dcaef37a22cce244e711dfb53dcde75e0362f4
Adam <ad1996@hotmail.co.uk>
2018-09-06 13:01:05 +0100

    Basic database communication commands added

--- DataController/DataController.cs
+++ DataController/DataController.cs
@@ -58,6 +58,45 @@ namespace DataController
         #endregion
 
         #region Data communciation
+        /// <summary>
+        /// Execute insert request, add data to the specified table
+        /// </summary>
+        /// <param name="tablename">Name of table in database</param>
+        /// <param name="newData">Data to be added</param>
+        /// <param name="insertColumn">Columns to be added to (null for all columns)</param>
+        private static void GeneralInsertNonQuery(string tableName, string[] newData, string[] insertColumn = null)
+        {
+            bool specifiedColumns = false;
+            if (insertColumn != null)
+                specifiedColumns = true;
+
+            if (specifiedColumns && newData.Count() != insertColumn.Count())
+                throw new ConstraintException("Columns do not match data");
+
+            //Construct command string
+            string commandString = SQLInsertQueryBuilder(newData.Count());
+            SqlCommand cmd = new SqlCommand(commandString, connection);
+
+            cmd.Parameters.Add(GenParameter("table", tableName));
+            if (specifiedColumns)
+            {
+                foreach (SqlParameter sqParam in GenIteratedParameters(insertColumn, "column"))
+                    cmd.Parameters.Add(sqParam);
+            }
+            foreach (SqlParameter sqParam in GenIteratedParameters(newData, "value"))
+                cmd.Parameters.Add(sqParam);
+
+            //Execute command
+            cmd.ExecuteNonQuery();
+        }
+        /// <summary>
+        /// Complete a fetch request for the specified columns via a search term
+        /// </summary>
+        /// <param name="tableName">Name of table in database</param>
+        /// <param name="requestedColumns">Columns to be fetched</param>
+        /// <param name="queryColumn">Column to query</param>
+        /// <param name="dataQuery">Data to query</param>
+        /// <returns>Database response</returns>
         private static List<string[]> GeneralFetchQuery(string tableName, string[] requestedColumns, string queryColumn, string dataQuery)
         {
             bool allColumns = false;
@@ -74,7 +113,7 @@ namespace DataController
             foreach (SqlParameter sqParam in GenParameters(parameterNames, parameterValues))
                 cmd.Parameters.Add(sqParam);
             //Build column parameters
-            foreach (SqlParameter sqParam in GenColumnParameters(requestedColumns))
+            foreach (SqlParameter sqParam in GenIteratedParameters(requestedColumns, "column"))
                 cmd.Parameters.Add(sqParam);
 
 
@@ -93,9 +132,103 @@ namespace DataController
             }
             return databaseResponse;
         }
+        /// <summary>
+        /// Complete an update request, edit existing database data
+        /// </summary>
+        /// <param name="tableName">Name of table in database</param>
+        /// <param name="queryColumn">Column to query</param>
+        /// <param name="dataQuery">Data to query</param>
+        /// <param name="updateColumns">Columns to update</param>
+        /// <param name="columnData">New data to replace existing</param>
+        private static void GeneralUpdateNonQuery(string tableName, string queryColumn, string dataQuery, string[] updateColumns, string[] columnData)
+        {
+            //Construct command string
+            string commandString = SQLUpdateQueryBuilder(updateColumns.Count());
+            SqlCommand cmd = new SqlCommand(commandString, connection);
+
+            //Construct paramters
+            string[] parameterNames = new string[3] { "table", "queryColumn", "dataQuery" };
+            string[] parameterValues = new string[3] { tableName, queryColumn, dataQuery };
+            foreach (SqlParameter sqParam in GenParameters(parameterNames, parameterValues))
+                cmd.Parameters.Add(sqParam);
+
+            //Build column and value parameters
+            foreach (SqlParameter sqParam in GenIteratedParameters(updateColumns, "column"))
+                cmd.Parameters.Add(sqParam);
+            foreach (SqlParameter sqParam in GenIteratedParameters(columnData, "newData"))
+                cmd.Parameters.Add(sqParam);
+
+            //Execute command
+            cmd.ExecuteNonQuery();
+        }
+        /// <summary>
+        /// Execute a delete command
+        ///</summary>
+        /// <param name="tableName">Name of table in database</param>
+        /// <param name="queryColumn">Column to query</param>
+        /// <param name="dataQuery">Data to query</param>
+        private static void GeneralDeleteNonQuery(string tableName, string queryColumn, string dataQuery)
+        {
+            string commandString = "DELETE FROM @table WHERE @queryColumn = @dataQuery";
+            SqlCommand cmd = new SqlCommand(commandString, connection);
+
+            //Construct paramters
+            string[] parameterNames = new string[3] { "table", "queryColumn", "dataQuery" };
+            string[] parameterValues = new string[3] { tableName, queryColumn, dataQuery };
+            foreach (SqlParameter sqParam in GenParameters(parameterNames, parameterValues))
+                cmd.Parameters.Add(sqParam);
+
+            //Execute command
+            cmd.ExecuteNonQuery();
+        }
+        #endregion
+
+        #region Communication commands
+
+
         #endregion
 
         #region Supporting functions
+        /// <summary>
+        /// Build's the SqlCommand string for n amount of columns for a insert request
+        /// </summary>
+        /// <param name="columnAmount">Number of columns</param>
+        ///<param name="specifiedColumns">Does the insert command require specified columns?</param>
+        /// <returns>SQL command string</returns>
+        private static string SQLInsertQueryBuilder(int columnAmount, bool specifiedColumns = false)
+        {
+            string SQLCommandString = "INSERT INTO @table ";
+            string columns = "(";
+            string values = "(";
+            for (int n = 0; n < columnAmount; n++)
+            {
+                columns += "@column" + n.ToString();
+                values += "@value" + n.ToString();
+                if (n != columnAmount)
+                {
+                    columns += ", ";
+                    values += ", ";
+                }
+                else
+                {
+                    columns += ") ";
+                    values += ")";
+                }
+            }
+            if (specifiedColumns)
+                SQLCommandString += columns;
+            SQLCommandString += "VALUES " + values + ";";
+
+            return SQLCommandString;
+        }
+
+        /// <summary>
+        /// Build's the SqlCommand string for n amount of columns for a fetch request
+        /// </summary>
+        /// <param name="columnAmount">Number of columns</param>
+        /// <param name="allColumns">Is the request for all columns?</param>
+        /// <param name="query">Does the command contain a query?</param>
+        /// <returns>SQL command string</returns>
         private static string SQLFetchQueryBuilder(int columnAmount, bool allColumns = false, bool query = true)
         {
             string SQLCommandString = "";
@@ -120,6 +253,36 @@ namespace DataController
             return SQLCommandString;
         }
 
+        /// <summary>
+        /// Build's the SqlCommand string for n amuont of columns for an update query
+        /// </summary>
+        /// <param name="columnAmount">Columns to update</param>
+        /// <param name="query">Does the command contain a query?</param>
+        /// <returns>SQL command string</returns>
+        private static string SQLUpdateQueryBuilder(int columnAmount, bool query = true)
+        {
+            string SQLCommandString = "UPDATE @table SET ";
+
+            for (int n = 0; n < columnAmount; n++)
+            {
+                SQLCommandString += "@column" + n.ToString() + " = @newData" + n.ToString();
+                if (n != columnAmount)
+                    SQLCommandString += ", ";
+            }
+            if (query) //If select criteria is required
+            {
+                SQLCommandString += " WHERE @queryColumn = @dataQuery ;";
+            }
+            return SQLCommandString;
+        }
+
+        /// <summary>
+        /// Generate a new sql parameter
+        /// </summary>
+        /// <param name="parameterName">Name of parameter</param>
+        /// <param name="parameterValue">Value of parameter</param>
+        /// <param name="dataType">Db datatype</param>
+        /// <returns>New SqlParamater</returns>
         private static SqlParameter GenParameter(string parameterName, string parameterValue, SqlDbType dataType = SqlDbType.NVarChar)
         {
             SqlParameter param = new SqlParameter(parameterName, SqlDbType.NVarChar)
@@ -128,6 +291,13 @@ namespace DataController
             };
             return param;
         }
+        /// <summary>
+        /// Genereate an array of sql parameters
+        /// </summary>
+        /// <param name="parameterName">Names of parameters</param>
+        /// <param name="parameterValue">Values of parameters</param>
+        /// <param name="dataType">Db datatype</param>
+        /// <returns></returns>
         private static SqlParameter[] GenParameters(string[] parameterName, string[] parameterValue, SqlDbType dataType = SqlDbType.NVarChar)
         {
             SqlParameter[] parameters = new SqlParameter[parameterName.Count()];
@@ -143,20 +313,29 @@ namespace DataController
             }
             return parameters;
         }
-
-        private static SqlParameter[] GenColumnParameters(string[] columns)
+        /// <summary>
+        /// Generate a new sql parameter foreach column requested
+        /// </summary>
+        /// <param name="columns">Columns requested</param>
+        /// <returns>Sql parameter array for each column</returns>
+        private static SqlParameter[] GenIteratedParameters(string[] columns, string iteratedString)
         {
             string[] paramName = new string[columns.Count()];
             string[] paramValue = new string[columns.Count()];
 
             for (int n = 0; n < columns.Count(); n++)
             {
-                paramName[n] = columns + n.ToString();
+                paramName[n] = "column" + n.ToString();
                 paramValue[n] = columns[n];
             }
 
             return GenParameters(paramName, paramValue);
         }
         #endregion
+    }
+
+    class DatabaseInformation
+    {
+        //Store information about database, tables, columns etc.
     }
 }
